1f8632ab2649ab17f0f2a8218f92621977a66e94
Dilmer Valecillos <dilmervst@gmail.com>
2023-03-16 15:50:44 -0600

    Added new project DeclaringVariables

---
+++ .NET/DeclaringVariables/DeclaringVariablesAndMore/Program.cs
@@ -0,0 +1,82 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Declaring Value Types");
+
+        // declare variables
+        int age = 25;
+        string fullName = "Michael Jordan";
+        double weight = 300D;
+        long distance = 10L;
+        GenderType genderType = GenderType.Male;
+        char gender = 'F';
+        float playerDistanceAtZ = 11.565433f;
+        decimal playerWorth = 1000.000M;
+
+        Console.WriteLine($"Player's Age: {age}");
+        Console.WriteLine($"Player's Full Name: {fullName}");
+        Console.WriteLine($"Player's Weight: {weight}");
+        Console.WriteLine($"Player's Distance Long: {distance}");
+        Console.WriteLine($"Player's Gender Type: {genderType}");
+        Console.WriteLine($"Player's Gender Char: {gender}");
+        Console.WriteLine($"Player's Distance Float: {playerDistanceAtZ}");
+        Console.WriteLine($"Player's Worth: {playerWorth}");
+
+        int x = 25, y = 39, z = 25;
+        int a, b = 37;
+        a = 35;
+
+        Console.WriteLine($"x: {x} y: {y} z: {z} a: {a} b: {b}");
+
+        Console.WriteLine("Declaring Arrays");
+
+        // single dimensional arrays
+        int[] ages = new int[] { 12, 13, 14, 15 };
+        Console.WriteLine($"{ages[0]} {ages[1]} {ages[2]} {ages[3]}");
+
+        // double dimensional arrays
+        // [rows,columns]
+        string[,] people = new string[,] { 
+                                            {"Michael", "Joe", "Amanda"},
+                                            {"Steve", "Vanesa", "Elon"},
+                                            {"Julio", "Brunella", "Dilmer"}
+                                         };
+        Console.WriteLine($"{people[2, 2]}");
+        Console.WriteLine($"{people[1, 0]}");
+        Console.WriteLine($"{people[0, 0]}");
+
+        Console.WriteLine("Value Types vs Reference Types");
+
+        int secondPlayerAge = 55;
+        Console.WriteLine($"Before -> secondPlayerAge: {secondPlayerAge}");
+        ValueTypeTesting(secondPlayerAge);
+        Console.WriteLine($"After -> secondPlayerAge: {secondPlayerAge}");
+
+        int[] secondSetOfAges = new int[]{ 12, 13, 14 };
+        Console.WriteLine($"Before -> secondSetOfAges: {secondSetOfAges[0]}");
+        ReferenceTypeTesting(secondSetOfAges);
+        Console.WriteLine($"After -> secondSetOfAges: {secondSetOfAges[0]}");
+
+    }
+
+    public static void ValueTypeTesting(int newAge)
+    {
+        newAge = 89;
+    }
+
+    public static void ReferenceTypeTesting(int[] ages)
+    {
+        if (ages.Length > 0)
+        {
+            ages[0] = 89;
+        }
+    }
+
+    enum GenderType
+    { 
+        Male = 0,
+        Female = 1,
+        NotProvided = -1
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,45 @@
-# UnityWithCSharpCourse
+# Unity With CSharp Course
 Unity With C# course supplemental source code examples
 
 💡 What is included in this repo? 
 * Includes Unity and .NET Console projects demonstrated with [Learn XR C# Fundamentals With Unity Course](https://www.learnxr.io/c-programming-fundamentals-with-unity)
+
+📚 Topic References:
+
+* Single dimensional arrays:
+    ```csharp
+    int[] array = new int[4];
+    array[0] = 1;
+    array[1] = 2;
+    array[2] = 3;
+    array[3] = 4;
+
+    int[] secondArray = new int[] { 2, 4, 8, 16 };
+    ```
+        *Visual representation of one dimensional arrays:*
+        |Index| Value|
+        |-----|------|
+        |  0  |  2   |
+        |  1  |  4   |
+        |  2  |  8   |
+        |  3  |  16  |
+
+* Two dimensional arrays:
+
+
+    ```csharp
+    //[rows, columns]
+    int[,] array = new int[3,3];
+    array[0, 0] = 1;
+    array[0, 1] = 5;
+
+    int[,] secondArray = {{ 1, 2, 3}, {3, 4, 5}};
+    Console.WriteLine(secondArray[0, 0]); //outputs 1
+    Console.WriteLine(secondArray[1, 1]); //outputs 4
+    Console.WriteLine(secondArray[1, 2]); //outputs 5
+```
+    *Visual representation of two dimensional arrays:*
+    |     |Column 0|Column 1|Column 2|
+    |-----|--------|--------|--------|
+    |Row 0|1       |2       |3       |
+    |Row 1|3       |4       |5       |
