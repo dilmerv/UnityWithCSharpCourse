7ce8e202c851cec05a4205a248ec100618899129
Dilmer Valecillos <dilmervst@gmail.com>
2023-03-17 15:41:10 -0600

    Added 2 new examples

---
+++ .NET/AllAboutMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection.Metadata;
+
+namespace AllAboutMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("All About Methods");
+
+            DisplayPlayerNameAndAge();
+            DisplayPlayerName();
+
+            string playerName = GetPlayerName();
+            Console.WriteLine($"Displaying The Player with Name: {playerName}");
+
+            DisplayPlayerNameAndAge("Karl Malone", 55);
+
+            Console.WriteLine($"Displaying The Player with Name: {GetPlayerName("_")}");
+        }
+
+        static void DisplayPlayerNameAndAge()
+        {
+            string playerName = "Michael Jordan";
+            string playerAge = "35";
+            Console.WriteLine($"Displaying The Age: {playerAge} Of A Player with Name: {playerName}");
+        }
+
+        static void DisplayPlayerNameAndAge(string playerName, int age)
+        {
+            Console.WriteLine($"Displaying The Age: {age} Of A Player with Name: {playerName}");
+        }
+
+        static void DisplayPlayerName()
+        {
+            string playerName = "Michael Jordan";
+            Console.WriteLine($"Displaying The Player with Name: {playerName}");
+        }
+
+        static string GetPlayerName(string separator)
+        {
+            return $"John{separator}Doe";
+        }
+
+        static string GetPlayerName()
+        {
+            string playerName = "John Doe";
+            return playerName;
+        }
+    }
+}
---
+++ .NET/IfElseSwitchStatements/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace IfElseSwitchStatements
+{
+    internal class Program
+    {
+        const decimal MAX_PLAYER_POINTS = 1000;
+        const decimal PLAYER_POINTS_TIER = 50;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If Else Switch Statements");
+            Console.WriteLine("\n");
+
+            IfElseFirstExample();
+            
+            Console.WriteLine("\n");
+
+            IfElseIfSecondExample();
+
+            Console.WriteLine("\n");
+
+            SwitchCaseExample("n");
+        }
+
+        private static void SwitchCaseExample(string gender)
+        {
+            gender = gender.ToUpper();
+
+            switch (gender)
+            {
+                case "F":
+                    Console.WriteLine("Female");
+                    break;
+                case "M":
+                    Console.WriteLine("Male");
+                    break;
+                case "N":
+                    Console.WriteLine("Not Provided");
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+        }
+
+        private static void IfElseIfSecondExample()
+        {
+            decimal currentPlayerPoints = 1000 + 51;
+
+            if (currentPlayerPoints == MAX_PLAYER_POINTS)
+            {
+                Console.WriteLine("Congratulation player you reached the max amount of points. You " +
+                        "now may advance to the next level !!!");
+            }
+            else if (currentPlayerPoints > MAX_PLAYER_POINTS &&
+                currentPlayerPoints <= MAX_PLAYER_POINTS + PLAYER_POINTS_TIER)
+            {
+                Console.WriteLine("Congratulation player you reached the max + 50 amount of points. You " +
+                            "now may advance to the next level !!!");
+            }
+            else if (currentPlayerPoints > MAX_PLAYER_POINTS + PLAYER_POINTS_TIER)
+            {
+                Console.WriteLine("Congratulation player you reached the ultimate max");
+            }
+        }
+
+        private static void IfElseFirstExample()
+        {
+            decimal currentPlayerPoints = 2000;
+
+            if (currentPlayerPoints >= MAX_PLAYER_POINTS)
+            {
+                Console.WriteLine("Congratulation player you reached the max amount of points. You " +
+                    "now may advance to the next level !!!");
+            }
+            else
+            {
+                Console.WriteLine($"Player keep working hard, you still " +
+                    $"have {MAX_PLAYER_POINTS - currentPlayerPoints} points remaining...");
+            }
+        }
+    }
+}
